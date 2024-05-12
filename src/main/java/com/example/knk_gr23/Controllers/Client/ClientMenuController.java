a20c9da9d6f03ed36a7ea530890ddf76e697c317
dreninimanaj <67548507+dreninimanaj@users.noreply.github.com>
2024-05-12 12:45:23 +0200

    b1

---
+++ src/main/java/com/example/knk_gr23/Controllers/Client/ClientMenuController.java
@@ -0,0 +1,18 @@
+package com.example.knk_gr23.Controllers.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button home_btn;
+    public Button apply_btn;
+    public Button profile_btn;
+    public Button logout_btn;
+    public Button apply_btn1;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+}
