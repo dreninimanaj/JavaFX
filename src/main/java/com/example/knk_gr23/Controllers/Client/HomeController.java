bcf07b50e5fb4e2cc2951edec098d3d8b930830f
dreninimanaj <67548507+dreninimanaj@users.noreply.github.com>
2024-05-13 16:33:35 +0200

    other changes

---
+++ src/main/java/com/example/knk_gr23/Controllers/Client/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.knk_gr23.Controllers.Client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController{
+    @FXML
+    public ListView loans_listview;
+
+    @FXML
+    public Button add_loan_button;
+}
---
+++ src/main/java/com/example/knk_gr23/database/DatabaseUtil.java
@@ -0,0 +1,25 @@
+package com.example.knk_gr23.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    private static String URL = "jdbc:mysql://localhost:3306/MenaxhimiKredise";
+    private static String USER = "root";
+    private static String PASSWORD = "dreni123";
+    private static Connection connection = null;
+
+    public static Connection getConnection(){
+        if(connection == null){
+            try {
+                connection = DriverManager.getConnection(
+                        URL, USER, PASSWORD
+                );
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return connection;
+    }
+}
