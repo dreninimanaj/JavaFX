6498743fff9ef3de9ec38433be451aacab079e26
dreninimanaj <67548507+dreninimanaj@users.noreply.github.com>
2024-05-15 20:24:45 +0200

    permirsim i navigator
    f

--- src/main/java/com/example/knk_gr23/App/Navigator.java
+++ src/main/java/com/example/knk_gr23/App/Navigator.java
@@ -4,6 +4,7 @@ import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ import java.io.IOException;
 public class Navigator {
     public final static String LOGIN_PAGE = "/Fxml/Login.fxml";
     public final static String HOME_PAGE = "/Fxml/Client/Client.fxml";
-    //public final static String CREATE_ACCOUNT_PAGE = "create_user_form.fxml";
+    public final static String ADMIN_PAGE = "/Fxml/Admin/Admin.fxml";
 
     // Method to navigate using an event
     public static void navigate(Event event, String form){
@@ -40,9 +41,20 @@ public class Navigator {
         try {
             Scene newScene = new Scene(loader.load());
             stage.setScene(newScene);
+            stage.setResizable(false);
             stage.show();
         } catch (IOException ioe) {
             ioe.printStackTrace(); // Better error handling or logging here
+        }
+    }
+
+    public static void navigate(BorderPane borderPane, String fxmlPath) {
+        FXMLLoader loader = new FXMLLoader(Navigator.class.getResource(fxmlPath));
+        try {
+            Node node = loader.load();
+            borderPane.setCenter(node);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
