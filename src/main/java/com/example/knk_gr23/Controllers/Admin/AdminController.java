ed2fb6d7f5b982d80c7b236a965a0a762be1dfff
dreninimanaj <67548507+dreninimanaj@users.noreply.github.com>
2024-05-25 14:17:17 +0200

    behet shfaqja e requestave nga userat

--- src/main/java/com/example/knk_gr23/Controllers/Admin/AdminController.java
+++ src/main/java/com/example/knk_gr23/Controllers/Admin/AdminController.java
@@ -1,12 +1,52 @@
 package com.example.knk_gr23.Controllers.Admin;
 
+import com.example.knk_gr23.Models.User;
+import com.example.knk_gr23.Services.ClientService;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class AdminController {
 
     @FXML
     private VBox requestsListView;
 
+    @FXML
+    public void initialize() throws SQLException {
 
+
+        try {
+            List<User> clients = ClientService.getAllClients();
+            displayClients(clients);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private void displayClients(List<User> usersList){
+        double totalHeight=300;
+        requestsListView.getChildren().clear();
+        for (User user : usersList) {
+            System.out.println(user);
+            try {
+                System.out.println(user.getId());
+                System.out.println();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/ClientsComponent.fxml"));
+                Pane busPane = loader.load();
+                ClientsComponentController clientsComponentController = loader.getController();
+                clientsComponentController.setData(user);
+                requestsListView.getChildren().add(busPane);
+
+                totalHeight += busPane.getPrefHeight() + requestsListView.getSpacing();
+
+            } catch (IOException e) {
+                System.out.println("eroor");
+            }
+            requestsListView.setPrefHeight(totalHeight);
+        }
+    }
 }
--- src/main/java/com/example/knk_gr23/Controllers/Admin/ClientsController.java
+++ src/main/java/com/example/knk_gr23/Controllers/Admin/ClientsController.java
@@ -1,18 +1,57 @@
 package com.example.knk_gr23.Controllers.Admin;
 
+import com.example.knk_gr23.Models.User;
+import com.example.knk_gr23.Services.ClientService;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class ClientsController implements Initializable {
-    public VBox clientsList;
+public class ClientsController  implements Initializable {
+
+
+    @FXML
+    private VBox clientsList;
+
+    private void displayClients(List<User> usersList){
+        double totalHeight=300;
+        clientsList.getChildren().clear();
+        for (User user : usersList) {
+            System.out.println(user);
+            try {
+                System.out.println(user.getId());
+                System.out.println();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Admin/ClientsComponent.fxml"));
+                Pane busPane = loader.load();
+                ClientsComponentController clientsComponentController = loader.getController();
+                clientsComponentController.setData(user);
+                clientsList.getChildren().add(busPane);
+
+                totalHeight += busPane.getPrefHeight() + clientsList.getSpacing();
+
+            } catch (IOException e) {
+                System.out.println("eroor");
+            }
+            clientsList.setPrefHeight(totalHeight);
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        try {
+            List<User> clients = ClientService.getAllClients();
+            displayClients(clients);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
