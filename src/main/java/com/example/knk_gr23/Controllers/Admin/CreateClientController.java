ef00795982b3eb4e46c153a6a7fbb22f73a023d5
elisagashi <elisagashi99@gmail.com>
2024-05-25 12:42:33 +0300

    behet shfaqja e kredive tek dashbordi i klientit

---
+++ src/main/java/com/example/knk_gr23/Controllers/Admin/CreateClientController.java
@@ -0,0 +1,20 @@
+package com.example.knk_gr23.Controllers.Admin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateClientController {
+    public TextField fName_fld;
+    public TextField lName_fld;
+    public TextField password_fld;
+    public TextField pID_fld;
+    public TextField hAddress_fld;
+    public TextField cHistory_fld;
+    public TextField empStatus_fld;
+    public TextField Income_fld;
+    public TextField cAmount_fld;
+    public TextField interest_fld;
+    public TextField dToI_FLD;
+    public TextField duration_fld;
+    public Button create_client_btn;
+}
