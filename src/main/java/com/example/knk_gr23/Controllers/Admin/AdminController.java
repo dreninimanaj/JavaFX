92a4d093cc9ebbc974a9811f564a50c621c085f1
dreninimanaj <67548507+dreninimanaj@users.noreply.github.com>
2024-05-25 14:45:32 +0200

    behet shfaqja e requestave nga userat

--- src/main/java/com/example/knk_gr23/Controllers/Admin/AdminController.java
+++ src/main/java/com/example/knk_gr23/Controllers/Admin/AdminController.java
@@ -1,52 +1,59 @@
 package com.example.knk_gr23.Controllers.Admin;
 
+import com.example.knk_gr23.App.SessionMenager;
+import com.example.knk_gr23.Models.Loan;
 import com.example.knk_gr23.Models.User;
 import com.example.knk_gr23.Services.ClientService;
+import com.example.knk_gr23.Services.LoanService;
+import com.example.knk_gr23.Services.PendingLoanService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class AdminController {
+public class AdminController implements Initializable {
 
     @FXML
     private VBox requestsListView;
 
-    @FXML
-    public void initialize() throws SQLException {
-
-
-        try {
-            List<User> clients = ClientService.getAllClients();
-            displayClients(clients);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    private void displayClients(List<User> usersList){
+    private void displayRequests(List<Loan> loansList){
         double totalHeight=300;
         requestsListView.getChildren().clear();
-        for (User user : usersList) {
-            System.out.println(user);
+        for (Loan loan : loansList) {
+            System.out.println(loan);
             try {
-                System.out.println(user.getId());
-                System.out.println();
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/ClientsComponent.fxml"));
+                System.out.println(loan.getLoan_id());
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Admin/RequestComponent.fxml"));
                 Pane busPane = loader.load();
-                ClientsComponentController clientsComponentController = loader.getController();
-                clientsComponentController.setData(user);
+                RequestComponentController requestComponentController = loader.getController();
+                requestComponentController.setData(loan);
                 requestsListView.getChildren().add(busPane);
 
                 totalHeight += busPane.getPrefHeight() + requestsListView.getSpacing();
 
             } catch (IOException e) {
-                System.out.println("eroor");
+                System.out.println("eroor1");
             }
             requestsListView.setPrefHeight(totalHeight);
         }
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            List<Loan> pendingLoans = PendingLoanService.getAllPendingLoans();
+            displayRequests(pendingLoans);
+        } catch (SQLException e) {
+            System.out.println("error");
+        }
+
     }
 }
